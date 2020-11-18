0230ca38e547d0616e1da6750d57e59fcaa73200
Alexandre Lafond <40358720+alexandrelafond@users.noreply.github.com>
2020-11-18 15:50:37 -0500

    [WEB-1513] Kill hanging process after five minutes (#2)
    * Kill hanging process after one hour
    
    * Better comment and reduce timeout to 5 minutes
    
    * Fix clearTimeout + Add tests for CLI
    
    Co-authored-by: Eric Matte <eric.matte@hotmail.ca>

--- package/bin/hello_pdf.js
+++ package/bin/hello_pdf.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-const path = require("path")
+const PROCESS_TIMEOUT = parseInt(process.env.PROCESS_TIMEOUT) || 5 * 60 * 1000; // 5 minutes
+
+const path = require("path");
 const args = require("yargs")
   .option("url", {
     alias: "i",
@@ -8,42 +10,42 @@ const args = require("yargs")
     type: "string",
     coerce: (url) => {
       if (url.indexOf("://") === -1) {
-        url = "file://" + path.resolve(url)
+        url = "file://" + path.resolve(url);
       }
       return url;
-    }
+    },
   })
   .option("output", {
     alias: "o",
     describe: "Provide a path to output file",
     type: "string",
-    coerce: path.resolve
+    coerce: path.resolve,
   })
   .option("header", {
     alias: "h",
     describe: "Header path",
-    coerce: path.resolve
+    coerce: path.resolve,
   })
   .option("footer", {
     alias: "f",
     describe: "Footer path",
-    coerce: path.resolve
+    coerce: path.resolve,
   })
   .option("pdf-options", {
-    describe: "JSON PDF Options passed directly to Puppeteer, see https://github.com/GoogleChrome/puppeteer/blob/v1.2.0/docs/api.md#pagepdfoptions",
+    describe:
+      "JSON PDF Options passed directly to Puppeteer, see https://github.com/GoogleChrome/puppeteer/blob/v1.2.0/docs/api.md#pagepdfoptions",
     type: "string",
-    coerce: JSON.parse
+    coerce: JSON.parse,
   })
   .option("extra-args", {
     describe: "Extra arguments passed to browser instance used by Puppeteer",
     type: "string",
-    coerce: JSON.parse
+    coerce: JSON.parse,
   })
   .demandOption(["url", "output"], "Please provide both url and output arguments to work with this tool")
-  .help()
-  .argv
+  .help().argv;
 
-const HelloPDF = require("../hello_pdf")
+const HelloPDF = require("../hello_pdf");
 
 const pdf = new HelloPDF({
   pageUrl: args.url,
@@ -52,13 +54,25 @@ const pdf = new HelloPDF({
   footerPath: args.footer,
   pdf: args.pdfOptions,
   extraArgs: args.extraArgs,
-})
+});
 
-pdf.generate()
+// Sometimes processes hang for 38h+, causing CPUs to top at 100%
+// and causes the application to be slower. This timeout manually
+// kills hanging processes after 5 minutes has elapsed.
+const killTimeout = setTimeout(() => {
+  console.error(`Process ${process.pid} killed after timer expired.`);
+  return process.kill(process.pid);
+}, PROCESS_TIMEOUT);
+
+pdf
+  .generate()
   .then((path) => {
-    console.log(path)
+    console.log(path);
   })
   .catch((err) => {
-    console.error(err.message)
-    process.exitCode = 1
+    console.error(err.message);
+    process.exitCode = 1;
   })
+  .finally(() => {
+    clearTimeout(killTimeout);
+  });
