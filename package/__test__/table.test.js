0d863752e6bebe9f3f7b9e2a42c2b33f89e2c125
Eric Matte <eric.matte@hotmail.ca>
2020-11-18 15:48:58 -0500

    Fix clearTimeout + Add tests for CLI

--- package/__test__/table.test.js
+++ package/__test__/table.test.js
@@ -1,15 +1,18 @@
 const HelloPDF = require("../hello_pdf");
 const path = require("path");
 const fs = require("fs");
+const { exec } = require("child_process");
+
+const outputPath = path.resolve(__dirname, "tmp/table.pdf");
+const pageUrl = "file://" + path.resolve(__dirname, "fixtures/table/index.html");
 
 test("generate a pdf", async () => {
-  const outputPath = path.resolve(__dirname, "tmp/table.pdf")
-  if (fs.existsSync(outputPath)) fs.unlinkSync(outputPath)
+  if (fs.existsSync(outputPath)) fs.unlinkSync(outputPath);
 
   const pdf = new HelloPDF({
     headerPath: path.resolve(__dirname, "fixtures/table/header.html"),
     footerPath: path.resolve(__dirname, "fixtures/table/footer.html"),
-    pageUrl: "file://" + path.resolve(__dirname, "fixtures/table/index.html"),
+    pageUrl,
     outputPath: outputPath,
     pdf: {
       landscape: true,
@@ -17,14 +20,49 @@ test("generate a pdf", async () => {
         top: "1cm",
         right: "1cm",
         bottom: "1cm",
-        left: "1cm"
-      }
-    }
-  })
+        left: "1cm",
+      },
+    },
+  });
 
   const pdfPath = await pdf.generate();
 
-  expect(pdfPath).toBe(outputPath)
+  expect(pdfPath).toBe(outputPath);
   expect(fs.existsSync(outputPath)).toBe(true);
-  fs.unlinkSync(outputPath)
+  fs.unlinkSync(outputPath);
+});
+
+test("cli can generate PDF", async () => {
+  const cmd = ["node", path.resolve(__dirname, "../../package/bin/hello_pdf"), "--url", pageUrl, "--o", outputPath];
+
+  await new Promise(async (resolve) => {
+    exec(cmd.join(" "), (_error, stdout, _stderr) => {
+      expect(stdout.trim()).toBe(outputPath);
+      resolve();
+    });
+  });
+
+  expect(fs.existsSync(outputPath)).toBe(true);
+  fs.unlinkSync(outputPath);
+});
+
+test("cli can timeout", async () => {
+  const cmd = [
+    "PROCESS_TIMEOUT=1",
+    "node",
+    path.resolve(__dirname, "../../package/bin/hello_pdf"),
+    "--url",
+    pageUrl,
+    "--o",
+    outputPath,
+  ];
+
+  await new Promise(async (resolve) => {
+    exec(cmd.join(" "), (_error, _stdout, stderr) => {
+      expect(stderr).toContain("killed after timer expired");
+      resolve();
+    });
+  });
+
+  expect(fs.existsSync(outputPath)).toBe(false);
 });
