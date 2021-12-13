51e5bc36e21b46c6f75c76f6bd7873b5469f0546
Philippe Chabot <philippe.chab@hotmail.com>
2021-12-13 13:37:22 -0500

    Add server folder and base express config

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+// import path from "path";
+
+const defaultPort = 4000;
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (_, res) => {
+  res.status(200).json({ message: "Everything's good!" });
+});
+
+const port = process.env.PORT || defaultPort;
+app.listen(port);
+console.log(`Running an API server at localhost:${port}`);
+
+// Uncomment to print routes
+// const filepath = path.join(__dirname, "./../routes.generated.txt");
+// require("express-print-routes")(app, filepath);
