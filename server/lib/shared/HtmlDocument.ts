33522fb0374d96b7910ffb32d5f335b58fdab626
Philippe Chabot <philippe.chab@hotmail.com>
2021-12-13 15:36:43 -0500

    Add bull dependency

---
+++ server/lib/shared/HtmlDocument.ts
@@ -0,0 +1,7 @@
+class HtmlDocument {
+  constructor(header, body, footer) {}
+
+
+}
+
+export default HtmlDocument;
---
+++ server/lib/shared/index.ts
@@ -0,0 +1,3 @@
+import HtmlDocument from "./HtmlDocument";
+
+export { HtmlDocument };
