5c85a28466b6fb41acad24a59959daed328e2dd7
Philippe Chabot <philippe.chab@hotmail.com>
2021-12-15 09:43:50 -0500

    Small tweak

--- server/src/convert/Post.ts
+++ server/src/convert/Post.ts
@@ -12,14 +12,14 @@ const post = async (req: Request, res: Response) => {
   const { filename, header, body, footer, webhookUrl, s3Url } = req.body;
 
   const document = new HtmlDocument(filename, body, { status: Status.Queued, webhookUrl, s3Url }, header, footer,);
-  let job: Job<HtmlDocument> | null  = await Producer.enqueue(document);
+  let job: Job<HtmlDocument> | null = await Producer.enqueue(document);
 
   if (!s3Url) {
     await job.finished();
     job = await GetJob(job.id);
-    const pdfBuffer = Buffer.from(job?.returnvalue.data);
+
     res.contentType("application/pdf");
-    return res.status(200).send(pdfBuffer);
+    return res.status(200).send(Buffer.from(job?.returnvalue.data));
   }
 
   res.status(200).json(new AsyncResult(job.id, filename, job.returnvalue.meta.status, webhookUrl, s3Url));
