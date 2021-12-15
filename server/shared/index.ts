e0bf1e8a6d87884b81b1846d14d09673b695f3dc
Philippe Chabot <philippe.chab@hotmail.com>
2021-12-15 12:02:31 -0500

    Move result classes into results folder

--- server/shared/index.ts
+++ server/shared/index.ts
@@ -1,9 +1,9 @@
-import AsyncResult from "./AsyncResult";
-import ErrorResult from "./ErrorResult";
 import HtmlDocument from "./HtmlDocument";
 import * as Http from "./Http";
 import Logger from "./Logger";
 import Queue from "./Queue";
 import { GetJob } from "./Utils";
+import AsyncResult from "./results/AsyncResult";
+import ErrorResult from "./results/ErrorResult";
 
 export { Queue, HtmlDocument, AsyncResult, ErrorResult, Logger, Http, GetJob };
