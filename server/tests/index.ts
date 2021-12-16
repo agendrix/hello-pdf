8ce87b70a4e8f0991108bdc7b99ad0e8ec3afe46
Philippe Chabot <philippe.chab@hotmail.com>
2021-12-16 10:52:00 -0500

    Fix sync e2e test

--- server/tests/index.ts
+++ server/tests/index.ts
@@ -1,17 +1,33 @@
+import dotenv from "dotenv";
 import FormData from "form-data";
-import { writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
+import path from "path";
+
+dotenv.config({ path: path.join(__dirname, "/../.env.test") });
+
+const port = process.env.PORT || 4000;
+const url = `http://localhost:${port}/convert`;
+const filename = "test";
+const file = readFileSync(path.resolve(__dirname, "./test.html"), "utf8");
 
 const payload = {
-  body: "<p>allo</p>",
-  header: "header",
-  footer: "footer",
-  filename: "filename",
+  filename: filename,
+  marginTop: "2cm",
+  marginRight: "2cm",
+  marginBottom: "2cm",
+  marginLeft: "2cm",
 };
 
 const form = new FormData();
 Object.entries(payload).forEach(([key, value]) => form.append(key, value));
 
-form.submit("http://localhost:4000/convert", function (_err, res) {
+form.append("body", Buffer.from(file), { filename: filename });
+
+form.submit(url, function (_err, res) {
+  if (_err) {
+    console.log(_err);
+  }
+
   const data: Array<any> = [];
   res.on("data", (chunk) => {
     data.push(chunk);
