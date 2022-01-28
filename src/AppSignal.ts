7d230c5ebe59bb0043389de0e4ac1906bd9657d3
Louis Boudreau <l.boudreau@agendrix.com>
2022-01-28 14:07:05 -0500

    Ignore health checks in AppSignal

--- src/AppSignal.ts
+++ src/AppSignal.ts
@@ -3,12 +3,11 @@ import { Appsignal } from "@appsignal/nodejs";
 const appSignalClient = new Appsignal({
   name: "HelloPdf",
   environment: process.env.APPSIGNAL_APP_ENV,
-  active: true,
+  active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined && process.env.NODE_ENV === "production",
   pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
   log: "stdout",
-  // TODO: Ignore health checks
-  ignoreActions: [],
-  debug: true,
+  ignoreActions: ["GET /health"],
+  debug: false,
 });
 
 export default appSignalClient;
