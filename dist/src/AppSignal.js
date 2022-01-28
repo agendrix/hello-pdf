66975df3254fc59a72d612f5f519856246948406
Louis Boudreau <l.boudreau@agendrix.com>
2022-01-28 13:05:01 -0500

    Force appsignal to true

--- dist/src/AppSignal.js
+++ dist/src/AppSignal.js
@@ -4,7 +4,7 @@ const nodejs_1 = require("@appsignal/nodejs");
 const appSignalClient = new nodejs_1.Appsignal({
     name: "HelloPdf",
     environment: process.env.APPSIGNAL_APP_ENV,
-    active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined && process.env.NODE_ENV === "production",
+    active: true,
     pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
     log: "stdout",
     // TODO: Ignore health checks
--- src/AppSignal.ts
+++ src/AppSignal.ts
@@ -3,7 +3,7 @@ import { Appsignal } from "@appsignal/nodejs";
 const appSignalClient = new Appsignal({
   name: "HelloPdf",
   environment: process.env.APPSIGNAL_APP_ENV,
-  active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined && process.env.NODE_ENV === "production",
+  active: true,
   pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
   log: "stdout",
   // TODO: Ignore health checks
