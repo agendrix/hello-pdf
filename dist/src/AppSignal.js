7cd3a1837d9334b0af16cc48ae4fb8f3c0df22d2
Louis Boudreau <l.boudreau@agendrix.com>
2022-01-28 12:26:02 -0500

    Debug AppSignal

--- dist/src/AppSignal.js
+++ dist/src/AppSignal.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const nodejs_1 = require("@appsignal/nodejs");
-const AppSignal = new nodejs_1.Appsignal({
+const appSignalClient = new nodejs_1.Appsignal({
     name: "HelloPdf",
     environment: process.env.APPSIGNAL_APP_ENV,
-    active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined,
+    active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined && process.env.NODE_ENV === "production",
     pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
     log: "stdout",
     // TODO: Ignore health checks
     ignoreActions: [],
+    debug: true,
 });
-exports.default = AppSignal;
+exports.default = appSignalClient;
--- src/AppSignal.ts
+++ src/AppSignal.ts
@@ -1,13 +1,14 @@
 import { Appsignal } from "@appsignal/nodejs";
 
-const AppSignal = new Appsignal({
+const appSignalClient = new Appsignal({
   name: "HelloPdf",
   environment: process.env.APPSIGNAL_APP_ENV,
-  active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined,
+  active: process.env.APPSIGNAL_PUSH_API_KEY !== undefined && process.env.NODE_ENV === "production",
   pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
   log: "stdout",
   // TODO: Ignore health checks
   ignoreActions: [],
+  debug: true,
 });
 
-export default AppSignal;
+export default appSignalClient;
