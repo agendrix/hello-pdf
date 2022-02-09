63aaa07a462e99474772088546a814fd2b9b0ba5
Louis Boudreau <l.boudreau@agendrix.com>
2022-02-08 20:23:40 -0500

    Test install with tarball

--- dist/shared/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status["Queued"] = "queued";
-    Status["Completed"] = "completed";
-    Status["Failed"] = "failed";
-})(Status = exports.Status || (exports.Status = {}));
