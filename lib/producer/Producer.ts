08546853fc0ef4944f4f9db700a1beb3b5199569
Louis Boudreau <l.boudreau@agendrix.com>
2022-02-10 11:56:40 -0500

    Add expiration to jobs to prevent redis bloat

--- lib/producer/Producer.ts
+++ lib/producer/Producer.ts
@@ -2,13 +2,19 @@ import { Queue as BullQueue, Job } from "bull";
 
 import { HtmlDocument, Queue } from "../../shared";
 
+const JOB_EXPIRATION_IN_SECONDS = process.env.JOB_EXPIRATION_IN_SECONDS
+  ? Number(process.env.JOB_EXPIRATION_IN_SECONDS)
+  : 60 * 60 * 24 * 7;
+
 class Producer {
   constructor(private queue: BullQueue<HtmlDocument>) {}
 
   async enqueue(document: HtmlDocument): Promise<Job<HtmlDocument>> {
     return this.queue.add(document, {
       attempts: 10,
-      backoff: { type: "exponential", delay: 500 }, // Max delay: https://github.com/OptimalBits/bull/blob/master/lib/backoffs.js#L12
+      backoff: { type: "exponential", delay: 500 }, // Max delay: https://github.com/OptimalBits/bull/blob/master/lib/backoffs.js#L12,
+      removeOnComplete: { age: JOB_EXPIRATION_IN_SECONDS },
+      removeOnFail: { age: JOB_EXPIRATION_IN_SECONDS },
     });
   }
 }
