e44ba1239ecee7d855672720301273d2852544c9
Louis Boudreau <l.boudreau@agendrix.com>
2022-02-16 15:09:03 -0500

    Clean queue after tests

--- tests/helpers/teardown.js
+++ tests/helpers/teardown.js
@@ -2,7 +2,7 @@ const Queue = require("../../dist/lib/Queue").default;
 const cleanProcesses = require("./cleanProcesses");
 
 module.exports = async function () {
-  Queue.obliterate({ force: true });
+  Queue.clean(0);
   global.workerProcess.kill("SIGTERM");
   cleanProcesses();
 };
